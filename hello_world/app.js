d5260e93f2c1902dbbfe733a80ae6b6895b88182
swetavsavarn <swetavsavarn@gmail.com>
2022-09-13 18:48:19 +0530

    ok

---
+++ hello_world/app.js
@@ -0,0 +1,60 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+   res.sendFile('E:/socket.io/hello_world/index.html');});
+users = [];
+io.on('connection', function(socket){
+   
+   socket.on('setUsername', function(data){
+      console.log(data);
+      if(users.indexOf(data) > -1){
+         socket.emit('userExists', data + ' username is taken!');
+      } else {
+         users.push(data);
+         socket.emit('userSet', {username: data});
+      }
+   });
+   socket.on('msg', function(data){
+      //Send message to everyone
+      io.sockets.emit('newmsg', data);
+   })
+});
+app.get('/joinroom',(req,res)=>{
+   users = [];
+   res.sendFile('E:/socket.io/hello_world/room.html')
+   io.on("connection", socket => {
+      // socket.join("some room");
+      socket.on('joinroom',(data)=>{
+         console.log(data)
+         if(users.indexOf(data.username) > -1){
+
+            socket.emit('userExists', data.username + ' username is taken!');
+         }
+         else
+         {
+            users.push(data.username);
+            
+          
+               socket.join(data.roomname);
+               var abc=data.roomname
+               socket.emit('userSet', {username: data.username});
+               socket.on('msg', function(data1){
+                  console.log(abc)
+                  // console.log(data1)
+                 
+                  io.to(abc).emit('newmsg', data1);
+               })
+             
+         }
+         
+
+      })
+    });
+
+
+})
+http.listen(3000, function(){
+   console.log('listening on localhost:3000');
+});
